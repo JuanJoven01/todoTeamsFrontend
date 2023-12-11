3352559cb7094ff6d9f7e0b5e5e8d51dd2d25048
JuanJoven01 <juanjov95@hotmail.com>
2023-12-11 10:09:43 -0500

    agregado envio de link y token para recuperar la contrasena

--- src/pages/logIn/index.jsx
+++ src/pages/logIn/index.jsx
@@ -1,5 +1,5 @@
-import { Link} from 'react-router-dom'
-import { useState } from 'react'
+import { Link, redirect} from 'react-router-dom'
+import { useState, useRef } from 'react'
 import { createPortal } from 'react-dom'
 
 import axios from 'axios'
@@ -16,12 +16,52 @@ const LogIn = () => {
   const [error, setError] = useState([false])
   const [loading, setLoading] = useState(false)
   const [successful, setSuccessful] = useState([false])
+
+  const nameRef = useRef(null)
+  const passwordRef = useRef(null)
+
+  // function to send the login Request
+
+  const sendLoginRequest = async (e) => {
+    e.preventDefault()
+    setLoading(true)
+
+    try {
+      const response = await axios.post('https://todo-teams-backend.onrender.com/login', 
+        {
+          name : nameRef.current.value,
+          password : passwordRef.current.value  
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'api-key': 'EstoEsSoloUnaPrueba',
+            'Accept': '*/*',
+          }
+        })
+      setLoading(false)
+      console.log(response.data)
+      if (response.data.error) {
+        setError([true, 'Error', response.data.error])
+      } else {
+        setSuccessful([true, 'Successful', 'Welcome ' + response.data.user.name + ' you are logged in'])
+        localStorage.setItem('token_todo_teams', response.data.token)
+        
+        
+        // pending to add the redirect to the tasks page
+      }
+    } catch (error) {
+      setLoading(false)
+      setError([true, 'Error', error.message])
+    }
+  }
+    
  
   // This is the view of the component
   return (
     <div className='login'>
       <h1 className='login__title'>
-        Here you can register to start using the app
+       Please insert your credentials
       </h1>
 
       {error[0] && createPortal(
@@ -47,31 +87,26 @@ const LogIn = () => {
         <input 
           type="name" 
           placeholder='Nickname / Alias'
-          name='name' />
+          name='name'
+          ref={nameRef} />
 
-        <input 
-          type="email" 
-          placeholder='Email'
-          name='mail' />
 
         <input 
           type="password" 
           placeholder='Password'
-          name='password' />
-
-        <input 
-          type="password" 
-          placeholder='Confirm Password'
-          name='confirm_password' />
+          name='password'
+          ref={passwordRef} />
         
-        <div className='form__button' >
-          <PurpleButton   text='Sign Up' />
+        <div className='form__button' onClick={sendLoginRequest} >
+          <PurpleButton   text='Log In' />
         </div>
         
 
       </form>
 
-      <Link to='/login' className='login__link' >Have you got a user? Log in</Link>
+      <Link to='/recovery-password' className='login__link' >Did you forget your password? Recovery</Link>
+
+      <Link to='/activate-user' className='login__link' >Your user is not activated yet? activate it here</Link>
 
       {loading && createPortal( <Loading />, document.body) }
 
--- src/pages/recoveryPassword/index.jsx
+++ src/pages/recoveryPassword/index.jsx
@@ -1,13 +1,107 @@
-import '../../app.css'
+import { Link, redirect} from 'react-router-dom'
+import { useState, useRef } from 'react'
+import { createPortal } from 'react-dom'
 
-function RecoveryPassword() {
+import axios from 'axios'
 
+import PurpleButton from '../../components/button'
+import ErrorDialog from '../../components/errorDialog'
+import SuccessfulDialog from '../../components/successfulDialog'
+import Loading from '../../components/loading'
+
+import './recovery.css'
+
+const RecoveryPassword = () => {
+
+  const [error, setError] = useState([false])
+  const [loading, setLoading] = useState(false)
+  const [successful, setSuccessful] = useState([false])
+
+  const emailRef = useRef(null)
+
+  // function to send the recovery Request
+
+  const sendRecoveryRequest = async (e) => {
+    e.preventDefault()
+    setLoading(true)
+
+    try {
+      const response = await axios.post('https://todo-teams-backend.onrender.com/recovery', 
+        {
+          mail : emailRef.current.value
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'api-key': 'EstoEsSoloUnaPrueba',
+            'Accept': '*/*',
+            'link': 'teams.juanjoven.com'
+          }
+        })
+      setLoading(false)
+      console.log(response.data)
+      if (response.data.error) {
+        setError([true, 'Error', response.data.error])
+      } else {
+        setSuccessful([true, 'Successful', 'We send a recovery link to your email'])
+        localStorage.setItem('token_todo_teams', response.data.token)
+        
+        
+        // pending to add the redirect to the tasks page
+      }
+    } catch (error) {
+      setLoading(false)
+      setError([true, 'Error', error.message])
+    }
+  }
+    
+ 
+  // This is the view of the component
   return (
-    <>
-      <div className='bg-cyan-300 h-60' > 
-      RecoveryPassword
-      </div>
-    </>
+    <div className='recovery'>
+      <h1 className='recovery__title'>
+       Please insert your credentials
+      </h1>
+
+      {error[0] && createPortal(
+        <ErrorDialog 
+          title={error[1]}
+          message={error[2]}
+          onClose={() => setError([false])}
+        />,
+        document.body
+      )}
+
+      {successful[0] && createPortal(
+        <SuccessfulDialog 
+          title={successful[1]}
+          message={successful[2]}
+          onClose={() => setSuccessful([false])}
+        />,
+        document.body
+      )}
+
+      <form className='recovery__form'>
+
+        <input 
+          type="email" 
+          placeholder='Insert your Email'
+          name='name'
+          ref={emailRef} />
+
+        
+        <div className='form__button' onClick={sendRecoveryRequest} >
+          <PurpleButton   text='Send Recovery Link' />
+        </div>
+        
+
+      </form>
+
+      <Link to='/recovery-password/change-pass' className='recovery__link' >Have you got a recovery token? Click here</Link>
+
+      {loading && createPortal( <Loading />, document.body) }
+
+    </div>
   )
 }
 
