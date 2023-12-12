2ea93115763b03cec2be0cdb430e1b7ddb0c9566
JuanJoven01 <juanjov95@hotmail.com>
2023-12-11 20:28:42 -0500

    iniciando creacion de las vistas de tareas

--- src/pages/tasks/index.jsx
+++ src/pages/tasks/index.jsx
@@ -1,13 +1,115 @@
-import '../../app.css'
+import { Link, redirect, useParams} from 'react-router-dom'
+import { useState, useRef, useEffect } from 'react'
+import { createPortal } from 'react-dom'
 
-function Tasks() {
+import axios from 'axios'
 
+import PurpleButton from '../../components/button'
+import ErrorDialog from '../../components/errorDialog'
+import SuccessfulDialog from '../../components/successfulDialog'
+import Loading from '../../components/loading'
+
+import './tasks.css'
+
+const Tasks = () => {
+
+  const [error, setError] = useState([false])
+  const [loading, setLoading] = useState(false)
+  const [successful, setSuccessful] = useState([false])
+
+  const [ownTasks, setOwnTasks] = useState([])
+
+  const getMyTasks = async () => {
+    try {
+      setLoading(true)
+      console.log(`Bearer ${localStorage.getItem('token_todo_teams')}`)
+      const response = await axios.get('https://todo-teams-backend.onrender.com/tasks', 
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'api-key': 'EstoEsSoloUnaPrueba',
+            'Accept': '*/*',
+            'Authorization': `Bearer ${localStorage.getItem('token_todo_teams')}`
+          }
+        })
+        
+      setLoading(false)
+      console.log(response.data)
+      if (response.data.error) {
+        setError([true, 'Error', response.data.error])
+      } else {
+        setOwnTasks(response.data)
+        
+        
+        // pending to add the redirect to the tasks page
+      }
+    } catch (error) {
+      setLoading(false)
+      setError([true, 'Error', error.message])
+    }
+  }
+
+  useEffect(() => {
+    getMyTasks(ownTasks)
+  },[])
+ 
+  // This is the view of the component
   return (
-    <>
-      <div className='bg-cyan-300 h-60' > 
-      Tasks
+
+    <div className='tasks'>
+      <h1 className='tasks__title'>
+        Here you can see your own tasks, the tasks of your team and create or update the tasks.
+      </h1>
+
+      <p className='tasks__subtitle'>
+        Your own tasks:
+      </p>
+
+      <div className='own_tasks' >
+        {ownTasks.map((task, index) => (
+          <div className='own_tasks__container' key={index}>
+            <p className='own_tasks__task__title'>
+              {task.title}
+            </p>
+            <p className='own_tasks__task__description'>
+              {task.description}
+            </p>
+            <p className='own_tasks__task__status'>
+              {task.status}
+            </p>
+            <p className='own_tasks__task__assigned'>
+              {task.assigned}
+            </p>
+            <p className='own_tasks__task__deadline'>
+              {task.deadline}
+            </p>
+          </div>
+        ))}
       </div>
-    </>
+
+      {error[0] && createPortal(
+        <ErrorDialog 
+          title={error[1]}
+          message={error[2]}
+          onClose={() => setError([false])}
+        />,
+        document.body
+      )}
+
+      {successful[0] && createPortal(
+        <SuccessfulDialog 
+          title={successful[1]}
+          message={successful[2]}
+          onClose={() => setSuccessful([false])}
+        />,
+        document.body
+      )}
+
+      
+
+      {loading && createPortal( <Loading />, document.body) }
+
+    </div>
   )
 }
 
